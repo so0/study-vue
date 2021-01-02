8e1d5197d6a617b8077576106d18d0e29666b991
so0 <syngshm@gmail.com>
2021-01-02 22:05:09 +0900

    v-model 속성과 submit이벤트 처리

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,19 +1,33 @@
 <template>
-  <form action="">
+  <form v-on:submit="submitForm">
     <div>
       <label for="username">id:</label>
-      <input id="username" type="text" />
+      <!-- v-model: 2way databinding -->
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw:</label>
-      <input type="text" id="password" />
+      <input type="password" id="password" v-model="password" />
     </div>
-    <button>login</button>
+    <button type="submit">login</button>
   </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function(event) {
+      event.preventDefault();
+      console.log("submitForm");
+    },
+  },
+};
 </script>
 
 <style></style>
