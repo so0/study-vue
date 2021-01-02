0f8bf4435de1519f67d64853317d92f61dfc7e47
so0 <syngshm@gmail.com>
2021-01-02 21:33:45 +0900

    싱글파일컴포넌트 event emit

--- learn-vue/vue-cli/src/App.vue
+++ learn-vue/vue-cli/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div>
     <!-- <app-header v-bind:프롭스 속성 이름 ="상위 컴포넌트의 데이터 이름"/> -->
-    <app-header v-bind:propsdata="str" />
-    {{ str }}
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr" />
   </div>
 </template>
 
@@ -23,6 +22,11 @@ export default {
   },
   components: {
     "app-header": AppHeader,
+  },
+  methods: {
+    renewStr: function() {
+      this.str = "hi!";
+    },
   },
 };
 </script>
--- learn-vue/vue-cli/src/components/AppHeader.vue
+++ learn-vue/vue-cli/src/components/AppHeader.vue
@@ -1,12 +1,19 @@
 <template>
   <header>
     <h1>{{ propsdata }}</h1>
+    <button v-on:click="sendEvent">send</button>
   </header>
 </template>
 
 <script>
 export default {
   props: ["propsdata"],
+  methods: {
+    sendEvent: function() {
+      console.log("sendEvent");
+      this.$emit("renew");
+    },
+  },
 };
 </script>
 
