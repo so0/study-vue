ee8e2a8d07b687c2c9654bf0875901f1fc4dc878
so0 <syngshm@gmail.com>
2021-01-02 21:59:03 +0900

    사용자 입력 폼 vue 프로젝트 생성, 마크업 작성

---
+++ vue-form/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">id:</label>
+      <input id="username" type="text" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input type="text" id="password" />
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
