95bf070a3719ca5c5de9d5b0a85f6ede0fe6b26b
so0 <syngshm@gmail.com>
2021-01-02 21:15:43 +0900

    싱글 파일 컴포넌트 체계에서 컴포넌트 등록하기

--- learn-vue/vue-cli/src/App.vue
+++ learn-vue/vue-cli/src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
+    <app-header />
     {{ str }}
   </div>
 </template>
 
 <script>
+import AppHeader from "./components/AppHeader.vue";
+
+// var AppHeader = {
+//   tempalte: "<header><h1>Header</h1></header>",
+// };
+// new Vue({
+//   data: { str: "hi" },
+// });
 export default {
   data: function() {
     return {
       str: "hi",
     };
+  },
+  components: {
+    "app-header": AppHeader,
   },
 };
 </script>
---
+++ learn-vue/vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
