c5214e9fa082afe449a24d9facdaea365b7285ba
so0 <syngshm@gmail.com>
2021-01-02 21:08:37 +0900

    싱글 파일 컴포넌트 - 데이터 바인딩

--- learn-vue/vue-cli/src/App.vue
+++ learn-vue/vue-cli/src/App.vue
@@ -1,34 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <!-- 컴포넌트 명명법 종류 -->
-    <!-- <hello-world></hello-world>
-    <HelloWorld></HelloWorld>
-    <HelloWorld /> -->
+  <div>
+    {{ str }}
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  // 인스턴스 옵션 속성, 컴포넌트 옵션 속성
-  name: "App",
-  components: {
-    HelloWorld,
-    // "hello-world": HelloWorld,
+  data: function() {
+    return {
+      str: "hi",
+    };
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
