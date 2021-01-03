69fab26006248ef4d97b07fab455d8b5092ccc09
so0 <syngshm@gmail.com>
2021-01-03 17:31:06 +0900

    TodoInput 컴포넌트 스타일링, 엔터 쳤을 때 이벤트 추가

--- learn-vue-2/vue-todo/src/components/TodoInput.vue
+++ learn-vue-2/vue-todo/src/components/TodoInput.vue
@@ -1,8 +1,10 @@
 <template>
-  <!-- 2 way binding -->
-  <div>
-    <input type="text" v-model="newTodoItem" />
-    <button v-on:click="addTodo">add</button>
+  <div class="inputBox shadow">
+    <!-- 2 way binding -->
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+    <span v-on:click="addTodo" class="addContainer">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -18,7 +20,9 @@ export default {
       console.log(this.newTodoItem);
       // 저장하는 로직
       localStorage.setItem(this.newTodoItem, this.newTodoItem);
-
+      this.clearInput();
+    },
+    clearInput: function() {
       // 초기화
       this.newTodoItem = "";
     },
@@ -26,4 +30,26 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
