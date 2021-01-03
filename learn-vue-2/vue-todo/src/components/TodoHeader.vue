7807c4572da2d608f8ad427e407dd78b83857824
so0 <syngshm@gmail.com>
2021-01-03 16:39:51 +0900

    TodoHeader 컴포넌ㅌ트 구현, App 컴포넌트 style추가

--- learn-vue-2/vue-todo/src/components/TodoHeader.vue
+++ learn-vue-2/vue-todo/src/components/TodoHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- html -->
-  <div>header</div>
+  <h1>TODO it!</h1>
 </template>
 
 <script>
@@ -9,6 +9,12 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* css */
+/* scoped : 컴포넌트 안에서만 유효한 스타일 속성.  */
+h1 {
+  color: #2f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
 </style>
