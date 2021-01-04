e9e6233fab3d99ce351c774edc87ade75177213c
so0 <syngshm@gmail.com>
2021-01-04 17:20:41 +0900

    TodoList 할일 삭제 기능

--- learn-vue-2/vue-todo/src/components/TodoList.vue
+++ learn-vue-2/vue-todo/src/components/TodoList.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
         {{ todoItem }}
-        <span class="removeBtn" v-on:click="removeTodo">
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
           <i class="fas fa-trash-alt "></i>
         </span>
       </li>
@@ -19,8 +19,10 @@ export default {
     };
   },
   methods: {
-    removeTodo: function() {
+    removeTodo: function(todoItem, index) {
       console.log("removeTodo");
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
     },
   },
   created: function() {
