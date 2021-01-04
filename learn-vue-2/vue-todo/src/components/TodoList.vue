d0ae65fba0cc8f8e6e43956056ae01c61cad6383
so0 <syngshm@gmail.com>
2021-01-04 17:04:24 +0900

    TodoList 컴포넌트 작성, v-for

--- learn-vue-2/vue-todo/src/components/TodoList.vue
+++ learn-vue-2/vue-todo/src/components/TodoList.vue
@@ -1,7 +1,33 @@
-<template> <div>list</div></template>
+<template>
+  <div>
+    <ul>
+      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+      </li>
+    </ul>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      todoItems: [],
+    };
+  },
+  created: function() {
+    //  vue 라이프 사이클 - 인스턴스가 생성 되자마자 호출됨.
+    console.log("created");
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        //console.log(localStorage.key(i));
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
+};
 </script>
 
 <style></style>
