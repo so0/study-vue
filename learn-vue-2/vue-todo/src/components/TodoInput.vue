287d7dc581c14bd2d9559e965800d3a199b87cc3
so0 <syngshm@gmail.com>
2021-01-04 23:07:40 +0900

    모달 컴포넌트 생성

--- learn-vue-2/vue-todo/src/components/TodoInput.vue
+++ learn-vue-2/vue-todo/src/components/TodoInput.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="inputBox shadow">
-    <!-- 2 way binding -->
-    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
-    <span v-on:click="addTodo" class="addContainer">
-      <i class="fas fa-plus addBtn"></i>
-    </span>
+  <div>
+    <div class="inputBox shadow">
+      <!-- 2 way binding -->
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" />
+      <span v-on:click="addTodo" class="addContainer">
+        <i class="fas fa-plus addBtn"></i>
+      </span>
+    </div>
+    <Modal v-if="showModal" @close="showModal = false">
+      <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+      <h3 slot="header">custom header</h3>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
 export default {
   props: ["propsdata"],
   data: function() {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
   methods: {
@@ -23,12 +34,16 @@ export default {
         // this.$emit('이벤트 이름', 인자1, 인자2, ...)
         this.$emit("addTodoItem", this.newTodoItem);
         this.clearInput();
+      } else {
       }
     },
     clearInput: function() {
       // 초기화
       this.newTodoItem = "";
     },
+  },
+  components: {
+    Modal: Modal,
   },
 };
 </script>
