a1367e0b4130a2cadd3382681eed3b6dd4fea61f
so0 <syngshm@gmail.com>
2021-01-10 22:56:47 +0900

    Toolbar 라우터 링크 스타일링

--- learn-vue-3/vue-news/src/App.vue
+++ learn-vue-3/vue-news/src/App.vue
@@ -3,6 +3,7 @@
     <!-- // 1 Strongly Recommended -->
     <Toolbar></Toolbar>
     <!-- // 2 Essential -->
+    <tool-bar></tool-bar>
     <router-view></router-view>
   </div>
 </template>
@@ -17,4 +18,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
--- learn-vue-3/vue-news/src/components/ToolBar.vue
+++ learn-vue-3/vue-news/src/components/ToolBar.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <router-link to="/news">News</router-link>
-    <router-link to="/ask">Ask</router-link>
+  <div class="header">
+    <router-link to="/news">News</router-link> | <router-link to="/ask">Ask</router-link> |
     <router-link to="jobs">Jobs</router-link>
   </div>
 </template>
@@ -10,4 +9,18 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.header {
+  color: white;
+  background-color: #42b883;
+  display: flex;
+  padding: 8px;
+}
+.header .router-link-exact-active {
+  color: #35495e;
+}
+
+.header a {
+  color: white;
+}
+</style>
