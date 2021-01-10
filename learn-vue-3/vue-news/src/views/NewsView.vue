2f3144038999b282869ed9807f261741b19afa05
so0 <syngshm@gmail.com>
2021-01-10 23:47:08 +0900

    axios 설치, news api 호출

--- learn-vue-3/vue-news/src/views/NewsView.vue
+++ learn-vue-3/vue-news/src/views/NewsView.vue
@@ -1,9 +1,31 @@
 <template>
-  <div>news</div>
+  <div>
+    <div v-for="user in users">{{ user }}</div>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    var vm = this;
+    // API 사이트 : https://github.com/tastejs/hacker-news-pwas/blob/master/docs/api.md
+    axios
+      .get('https://api.hnpwa.com/v0/news/1.json')
+      .then(function(response) {
+        console.log(response);
+        vm.users = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
 
 <style></style>
