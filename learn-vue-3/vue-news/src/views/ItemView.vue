1d021cb3c52529410828b51d455d4f374ad00caf
so0 <syngshm@gmail.com>
2021-01-12 16:03:32 +0900

    질문 상세 페이지 스타일링, v-html

--- learn-vue-3/vue-news/src/views/ItemView.vue
+++ learn-vue-3/vue-news/src/views/ItemView.vue
@@ -2,19 +2,28 @@
   <div>
     <section>
       <!-- 질문 상세 정보 -->
-      <div>
-        <div>User</div>
-      </div>
-      <div>
-        <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
+      <div class="user-container">
+        <div>
+          <i class="fas fa-user"></i>
+        </div>
+        <div class="user-description">
+          <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link>
+          <div class="time">
+            {{ fetchedItem.time_ago }}
+          </div>
+        </div>
       </div>
       <h2>{{ fetchedItem.title }}</h2>
     </section>
     <section>
       <!-- 질문 댓글 -->
     </section>
-    <div>{{ this.fetchedItem.content }}</div>
-    <small>{{ this.fetchedItem.time_ago }}</small>
+    <section>
+      <div v-html="fetchedItem.content"></div>
+      <div v-for="cmt in fetchedItem.comments">
+        <div v-html="cmt.content"></div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -31,4 +40,19 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
