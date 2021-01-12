82b3b1cda25d1837450afd9a2f632be98a8304ab
so0 <syngshm@gmail.com>
2021-01-12 15:08:08 +0900

    Userview  스토어 정보 - computed 속성 사용

--- learn-vue-3/vue-news/src/views/UserView.vue
+++ learn-vue-3/vue-news/src/views/UserView.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-    <p>name : {{ this.$store.state.user.id }}</p>
-    <p>karma : {{ this.$store.state.user.karma }}</p>
-    <p>created : {{ this.$store.state.user.created }}</p>
+    <p>name : {{ userInfo.id }}</p>
+    <p>karma : {{ userInfo.karma }}</p>
+    <p>created : {{ userInfo.created }}</p>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    },
+  },
   created() {
     console.log(this.$route);
     const userName = this.$route.params.id;
