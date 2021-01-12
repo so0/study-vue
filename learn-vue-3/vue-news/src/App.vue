e3ad77ca60dae57f0a493cc8b5ea4b8c6e1cefd4
so0 <syngshm@gmail.com>
2021-01-12 16:16:54 +0900

    라우터 - 트렌지션 적용

--- learn-vue-3/vue-news/src/App.vue
+++ learn-vue-3/vue-news/src/App.vue
@@ -4,7 +4,9 @@
     <Toolbar></Toolbar>
     <!-- // 2 Essential -->
     <!-- <tool-bar></tool-bar> -->
-    <router-view></router-view>
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -22,5 +24,14 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* router transition */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
