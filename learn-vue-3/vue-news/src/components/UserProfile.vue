d6c2463082d949f67939f87e75ff6fa6d4570eb6
so0 <syngshm@gmail.com>
2021-01-12 23:39:22 +0900

    UserProfile 공통 컴포넌트 생성

---
+++ learn-vue-3/vue-news/src/components/UserProfile.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="user-container">
+    <div>
+      <i class="fas fa-user"></i>
+    </div>
+    <div class="user-description">
+      <!-- <router-link :to="`/user/${fetchedItem.user}`">{{ fetchedItem.user }}</router-link> -->
+      <div class="time">
+        <!-- {{ fetchedItem.time_ago }} -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {},
+};
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.fa-user {
+  font-size: 2.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
--- learn-vue-3/vue-news/src/views/UserView.vue
+++ learn-vue-3/vue-news/src/views/UserView.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
+    <user-profile />
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
+import UserProfile from '../components/UserProfile.vue';
 export default {
-  computed: {
-    userInfo() {
-      return this.$store.state.user;
-    },
+  components: {
+    UserProfile,
   },
-  created() {
-    console.log(this.$route);
-    const userName = this.$route.params.id;
-    console.log(userName);
-    // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
-    this.$store.dispatch('FETCH_USER', userName);
-  },
+  // computed: {
+  //   userInfo() {
+  //     return this.$store.state.user;
+  //   },
+  // },
+  // created() {
+  //   console.log(this.$route);
+  //   const userName = this.$route.params.id;
+  //   console.log(userName);
+  //   // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
+  //   this.$store.dispatch('FETCH_USER', userName);
+  // },
 };
 </script>
 
