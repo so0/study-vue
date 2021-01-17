43b7f065d407a0efc5c5b97a59e6552abcb51d22
so0 <syngshm@gmail.com>
2021-01-17 21:22:08 +0900

    Controlled Component - 구현 방법과 활용처  데이트피커, 모달, 입력 폼.. 등

--- learn-vue-3/design/controlled/src/App.vue
+++ learn-vue-3/design/controlled/src/App.vue
@@ -1,11 +1,15 @@
 <template>
  <!-- 잘못된 방법 -->
-  <check-box :checked="checked"></check-box>
+  <!-- <check-box :checked="checked"></check-box> -->
+
+  <check-box v-model="checked"></check-box>
 </template>
 
 <script>
 import CheckBox from './components/CheckBox.vue';
 
+// 세부 컴포넌트 쪼갤 때 유용한 방식.
+// 데이트피커, 모달, 입력 폼.. 등
 export default {
   components: {
     CheckBox
--- learn-vue-3/design/controlled/src/components/CheckBox.vue
+++ learn-vue-3/design/controlled/src/components/CheckBox.vue
@@ -1,10 +1,18 @@
 <template>
 <!-- props를 바꾸지말라는 경고메시지가 뜸 -->
-  <input type="checkbox" v-model="checked">
+  <input type="checkbox" :value="value" @click="toggleCheckBox">
 </template>
 
 <script>
 export default {
-  props: ['checked']
+  // @input 이벤트  - 올릴때
+  // :value 값  - 내려온 값
+  props: ['value'],
+  methods: {
+    toggleCheckBox() {
+      // 기본적인 컴포넌트 통신 방식
+      this.$emit('input', !this.value)
+    }
+  }
 }
 </script>
